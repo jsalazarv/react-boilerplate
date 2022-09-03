24b50eaa95eac558eaa2d962581d07a440368186
Juan Salazar <jsalazarv8@gmail.com>
2022-09-03 01:26:45 -0500

    Environment variable for deploy path

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(() => {
+  const path = process.env.PROJECT_PATH;
+
   return {
     plugins: [react()],
-    base: mode === 'deploy' ? '/profile-card/' : '/',
+    base: path ? path : '/',
   };
 });
